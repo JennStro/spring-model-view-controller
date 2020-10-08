9c288ae5a09eabccd4cf7119fca1bfdc206189ea
JennStro <jennys1994@gmail.com>
2020-10-08 10:03:35 +0200

    Add project

---
+++ src/main/java/com/example/springwebexperiment/SpringWebExperimentApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebexperiment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebExperimentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringWebExperimentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springwebexperiment/SpringWebExperimentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebexperiment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebExperimentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
